220865d34943ad38448bbd8d598ef18268ac3493
LiamF76 <148378361+LiamF76@users.noreply.github.com>
2024-11-07 17:58:58 -0500

    Add files via upload

---
+++ gitTest/main.py
@@ -0,0 +1,14 @@
+
+def my_function():
+    print("Hello from a function")
+    # This is a comment
+
+    # Print a message to the console
+    print("Hello, World!")
+
+    # Get input from the user
+    name = input("What's your name? ")
+    print("Nice to meet you,", name)
+
+
+my_function()
